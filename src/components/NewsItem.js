dab66ead02b214d021e2b71846ad6d688e02cc8d
Ashutosh <54012311+ashvtosh@users.noreply.github.com>
2023-03-10 00:25:47 +0530

    Add files via upload

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,20 +2,22 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-    let {title, description, imageUrl, newsUrl} = this.props;
-    return (
-      <div className="my-3">
-        <div className="card" style={{width: "18rem"}}>
-            <img src={!imageUrl?"https://www.reuters.com/pf/resources/images/reuters/reuters-default.png?d=132":imageUrl} className="card-img-top" alt="..."/>
-            <div className="card-body">
-                <h5 className="card-title">{title}...</h5>
-                <p className="card-text">{description}...</p>
-                <a rel="noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+        let {title, description, imageUrl, newsUrl, author, date, source} = this.props;
+        return (
+            <div className="my-3">
+                <div className="card">
+                <span className="position-absolute top-0 translate-middle badge rounded-pill bg-success" style={{left:'90%', zIndex:'1'}}>{source}</span>
+                    <img src={!imageUrl?"https://fdn.gsmarena.com/imgroot/news/21/08/xiaomi-smart-home-india-annoucnements/-476x249w4/gsmarena_00.jpg":imageUrl} className="card-img-top" alt="..."/>
+                    <div className="card-body">
+                        <h5 className="card-title">{title}</h5>
+                        <p className="card-text">{description}</p>
+                        <p className="card-text"><small className="text-muted">By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
+                        <a rel="noopener noreferrer" href={newsUrl} target="_blank" className="btn btn-sm btn-dark">Read More</a>
+                    </div>
+                </div>
             </div>
-            </div>
-      </div>
-    )
-  }
+        )
+    }
 }
 
 export default NewsItem
